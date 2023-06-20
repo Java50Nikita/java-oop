fa614cbc7c1a1a67bbcd042565c1c847a95377ff
Mikita Makouski <makofthesky@gmail.com>
2023-06-20 18:51:58 +0300

    HW #7

---
+++ src/telran/numbers/test/CalculatorTest.java
@@ -0,0 +1,42 @@
+package telran.numbers.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.function.BinaryOperator;
+
+import org.junit.jupiter.api.Test;
+
+import telran.numbers.CalcData;
+import telran.numbers.Calculator;
+
+class CalculatorTest {
+
+	@Test
+	void test() {
+		BinaryOperator<Double> bo = (a,b) -> a + b;
+		assertEquals(30,bo.apply(10.0, 20.0));
+		DoubleBinaryOperator[] operators = {
+				(a, b) -> a + b,
+				(a, b) -> a - b,
+				(a, b) -> a * b,
+				(a, b) -> a / b
+		};
+		double[] results = {30, 10, 200, 2};
+		double op1 = 20;
+		double op2 = 10;
+		for(int i = 0; i < operators.length; i++) {
+			assertEquals(results[i], operators[i].apply(op1, op2));
+		}
+	}
+@Test
+void calculateTest() {
+	assertEquals(20, Calculator.calculate(new CalcData(40, 20, '-')));
+	assertEquals(0.5, Calculator.calculate(new CalcData(1, 2, '/')));
+	assertEquals(1, Calculator.calculate(new CalcData(0.5, 2, '*')));
+	assertEquals(0.74, Calculator.calculate(new CalcData(0.5, 0.24, '+')));
+}
+}
+
+interface DoubleBinaryOperator extends BinaryOperator<Double> {
+	
+}
